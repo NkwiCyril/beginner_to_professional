d6534aaf96027bc25bd5dba627ba4f3266bc7dff
NkwiCyril <akinimbomnkwi@gmail.com>
2023-12-03 03:34:56 +0100

    find sort indexOf reverse

--- js_multiple_values/array_methods.js
+++ js_multiple_values/array_methods.js
@@ -68,7 +68,7 @@ console.log(newArr);
 // using the find() in-built method to check whether an element is present in the array
 
 names = ['Nkwi', 'Cyril', 'Akinimbom']
-let findValue = names.find((e) => e === 'Cyril')
+let findValue = names.find((e) => e === 'hello')
 
 console.log(findValue);
 
@@ -81,5 +81,28 @@ console.log(firstName);
 // using the sort() in-built method
 
 let sortMe = [1,7,84,3,5]
-let sortedArray = sortMe.sort((a, b) => a - b)
+let sortedArray = sortMe.sort()
 console.log(sortMe);
+
+// working with the splice(), shift() and delete methods
+
+let testArray = [1,2,4,5,6]
+
+testArray.splice(0,0) // removes 1 from index 0 to the last index
+// testArray.shift() // removes the first element of the array
+delete testArray[0] // removes the first element of the array but does not tamper with the available space or length of that array
+console.log(testArray);
+
+// REVERSING AN ARRAY
+// using the reverse() method to reverse the content of an array
+
+let reverseMe = [1,4,2,5,6] // reverses from index 0 => N to N => 0
+reverseMe.reverse()
+console.log(reverseMe);
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// PRACTICE EXERCISE 3.2
