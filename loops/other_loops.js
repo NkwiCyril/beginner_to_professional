4d8701af2370b158146ed1a4918b7d451a982eab
NkwiCyril <akinimbomnkwi@gmail.com>
2024-01-25 22:41:51 +0100

    some linting

--- loops/other_loops.js
+++ loops/other_loops.js
@@ -1,4 +1,3 @@
-
 var arr = ["Mill", "Jof", "Max", "Fineas"];
 
 let car = {
@@ -8,7 +7,6 @@ let car = {
   color: "black",
 };
 
-
 for (let prop in car) {
   console.log(car[prop]);
 }
@@ -16,7 +14,7 @@ for (let prop in car) {
 // converting from objects to an array
 // lets convert car to an array
 
-let carKeyArray = Object.keys(car); // gets the properties of object car and displays 
+let carKeyArray = Object.keys(car); // gets the properties of object car and displays
 console.log(carKeyArray);
 
 let carValueArray = Object.values(car);
@@ -25,14 +23,48 @@ console.log(carValueArray);
 // now let us put key: value keys together
 
 for (let i = 0; i < carKeyArray.length; i++) {
-    
-    console.log( carKeyArray[i] + ": " + carValueArray[i] );
-
+  console.log(carKeyArray[i] + ": " + carValueArray[i]);
 }
 
 // create array for each key-value pair in the object placed as input
-var carKeyValue = Object.entries(car); 
+var carKeyValue = Object.entries(car);
 
-for (const [key, value] of carKeyValue ) {
-    console.log(key + ": " + value );
+for (const [key, value] of carKeyValue) {
+  console.log(key + ": " + value);
+}
+
+// now, using break and continue statements when looping through objects
+//consider the various cars below
+let cars = [
+  {
+    model: "Golf",
+    make: "Volkswagen",
+    year: 1999,
+    color: "black",
+  },
+  {
+    model: "Picanto",
+    make: "Kia",
+    year: 2020,
+    color: "red",
+  },
+  {
+    model: "Peugeot",
+    make: "208",
+    year: 2021,
+    color: "black",
+  },
+  {
+    model: "Fiat",
+    make: "Punto",
+    year: 2020,
+    color: "black",
+  },
+];
+
+for (let i = 0; i < cars.length; i++) {
+    if (cars[i].year >= 2020 && cars[i].color == "black" ) {
+        console.log("I have found my new car: ", cars[i]);
+        break;
+    }
 }
