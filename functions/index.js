e150dcb941db6f531a9c04244ea4e34fa1935891
NkwiCyril <akinimbomnkwi@gmail.com>
2024-01-26 19:31:46 +0100

    function-containing variable

--- functions/index.js
+++ functions/index.js
@@ -12,3 +12,11 @@ function newFunc(x) {
 const greetMe = newFunc(2);
 const value = greetMe(90);
 console.log(value);
+
+
+// variable containing a function to print some information on console
+let varContainingFunction = function() { 
+  let varInFunction = "I'm in a function."; 
+  console.log("hi there!", varInFunction); 
+}; 
+varContainingFunction(); 
