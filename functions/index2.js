6e787dabf779bad407b5f29d8458e9af29f329b7
NkwiCyril <akinimbomnkwi@gmail.com>
2024-02-04 23:54:58 +0100

    rest, spread and return values

--- functions/index2.js
+++ functions/index2.js
@@ -6,5 +6,17 @@ names.forEach((name) => {
   console.log("Hello " + name[0].toUpperCase() + name.slice(1));
 });
 
-// spread operator to get all names in names list 
-console.log(...names);
+// spread operator to get all names in names list
+console.log([{ ...names }]);
+
+var addTwoNumbers = (x, y) => x + y;
+
+let result = addTwoNumbers(4, 5);
+console.log(result);
+
+let resultsArr = [];
+for (let i = 0; i < 10; i++) {
+  let result = addTwoNumbers(i, 2 * i);
+  resultsArr.push(result);
+}
+console.log(resultsArr);
